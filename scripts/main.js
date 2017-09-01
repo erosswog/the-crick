fb279c90991b8afa5cfc3d9c2e2f0e0bcc9f9faa
erosswog <erosswog@gmail.com>
2017-09-01 00:56:48 -0400

    Updates to index.html and main.js to load the leaderboard from the Firebase DB.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// Template for leaderboard Row
+TheCrick.LEADERBOARD_ROW_TEMPLATE =
+  '<td class="rank"></td>' +
+  '<td class="Player"></td>' +
+  '<td class="Total"></td>' +
+  '<td class="Rd1"></td>' +
+  '<td class="Rd2"></td>' +
+  '<td class="Rd3"></td>';
+
 // Initializes the App
 function TheCrick() {
   this.checkSetup();
@@ -9,10 +18,13 @@ function TheCrick() {
   this.userName = document.getElementById("user-name");
   this.signInButton = document.getElementById('sign-in');
   this.signOutButton = document.getElementById('sign-out');
+  this.leaderboard = document.getElementById('leaderboard');
+  this.rank = 1;
 
   this.signOutButton.addEventListener('click', this.signOut.bind(this));
   this.signInButton.addEventListener('click', this.signIn.bind(this));
   this.initFirebase();
+  this.loadLeaderboard();
 }
 
 // Sets up shortcuts to Firebase features and initiate firebase auth.
@@ -127,6 +139,57 @@ TheCrick.prototype.checkSetup = function() {
   }
 };
 
+// Loads the leaderboard from the database
+TheCrick.prototype.loadLeaderboard = function() {
+  // Reference to the /messages/ database path.
+  this.leaderboardRef = this.database.ref('leaderboard');
+  // Make sure we remove all previous listeners
+  this.leaderboardRef.off();
+
+  // Loads the last 12 messages and listen for new ones.
+  var setEntry = function(data) {
+    var val = data.val();
+    this.displayLeaderboardEntry(data.key, val.Total, val.rd1, val.rd2, val.rd3);
+  }.bind(this);
+  this.leaderboardRef.on('child_added', setEntry);
+  this.leaderboardRef.on('child_changed', setEntry);
+};
+
+// Displays the Leaderboard Entry in the table
+TheCrick.prototype.displayLeaderboardEntry = function(name, total, rd1, rd2, rd3) {
+  // First need to create objects for each golfer.
+  // Then need to store the golfers in an array
+  // Then need to sort the golfers array by score for display.
+    // Or make it easier on ourselves and have the serverside handle it by insertion
+  var row = document.createElement('tr');
+  var cell1 = document.createElement('td');
+  var tn1 = document.createTextNode(this.rank++);
+  cell1.appendChild(tn1);
+  var cell2 = document.createElement('td');
+  cell2.setAttribute("class", "mdl-data-table__cell--non-numeric");
+  var tn2 = document.createTextNode(name);
+  cell2.appendChild(tn2);
+  var cell3 = document.createElement('td');
+  var tn3 = document.createTextNode(total);
+  cell3.appendChild(tn3);
+  var cell4 = document.createElement('td');
+  var tn4 = document.createTextNode(rd1);
+  cell4.appendChild(tn4);
+  var cell5 = document.createElement('td');
+  var tn5 = document.createTextNode(rd2);
+  cell5.appendChild(tn5);
+  var cell6 = document.createElement('td');
+  var tn6 = document.createTextNode(rd3);
+  cell6.appendChild(tn6);
+  row.appendChild(cell1);
+  row.appendChild(cell2);
+  row.appendChild(cell3);
+  row.appendChild(cell4);
+  row.appendChild(cell5);
+  row.appendChild(cell6);
+  this.leaderboard.appendChild(row);
+};
+
 window.onload = function() {
   window.thecrick = new TheCrick();
-}
+};
