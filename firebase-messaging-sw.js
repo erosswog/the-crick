8b582da634791bf6a7a549f6e3bd5ac46ff05249
erosswog <erosswog@gmail.com>
2018-08-22 23:53:49 -0400

    Added 2017 results

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('/__/firebase/3.8.0/firebase-app.js');
-importScripts('/__/firebase/3.8.0/firebase-messaging.js');
+importScripts('/__/firebase/4.2.0/firebase-app.js');
+importScripts('/__/firebase/4.2.0/firebase-messaging.js');
 importScripts('/__/firebase/init.js');
 
 firebase.messaging();
